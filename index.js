1c35ce693857b8843386e866b5648af1e6f95735
satyajeetshahane@gmail.com <satyajeet@satyajeets-mbp.wireless.rit.edu>
2015-09-23 11:13:21 -0400

    cache dir creation

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var request = require('request');
 var _ = require('underscore');
 var validator = require('url-validator');
 
+//make cache dir if not present
+try {
+    fs.readdirSync('cache');
+} catch(e) {
+    fs.mkdirSync('cache'); 
+}
+
 request('http://www.reddit.com/r/pics/new.json', function(error, response, data) {
     if (!error && response.statusCode == 200) {
         data = JSON.parse(data);
@@ -11,7 +18,7 @@ request('http://www.reddit.com/r/pics/new.json', function(error, response, data)
             var fileName = fileNameArr[fileNameArr.length-1];
             if (validator(ele.data.thumbnail)) {
                 console.log("fetching: " + ele.data.thumbnail);
-                request(ele.data.thumbnail).pipe(fs.createWriteStream(fileName));
+                request(ele.data.thumbnail).pipe(fs.createWriteStream('cache/' + fileName));
             }
         });
     } else {
